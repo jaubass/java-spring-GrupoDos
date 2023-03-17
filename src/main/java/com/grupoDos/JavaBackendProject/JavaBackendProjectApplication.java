72231e6a5ecee20d8a174b1061ca8ee28ad58c1f
Jau <jaubass@gmail.com>
2023-03-17 15:30:50 +0100

    project structure

---
+++ src/main/java/com/grupoDos/JavaBackendProject/JavaBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.grupoDos.JavaBackendProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupoDos/JavaBackendProject/JavaBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupoDos.JavaBackendProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
