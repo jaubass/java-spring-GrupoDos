0099d54bd356cef9ea249092dec4a4115ae409e7
Jau <jaubass@gmail.com>
2023-04-02 16:21:23 +0200

    Lits of restaurants

--- src/main/java/com/grupoDos/JavaBackendProject/controller/RestaurantController.java
+++ src/main/java/com/grupoDos/JavaBackendProject/controller/RestaurantController.java
@@ -1,14 +1,26 @@
 package com.grupoDos.JavaBackendProject.controller;
 
+import com.grupoDos.JavaBackendProject.model.Restaurant;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class RestaurantController {
 
     @GetMapping("/restaurants")
     public String index(Model model) {
+        List<Restaurant> restaurants = new ArrayList<Restaurant>();
+        restaurants.add(new Restaurant (null, "Piamonte", "piamonte@email.com", 999555111, "Calle Mar 21", "Bilbao", 12380, "www.piamonte.com"));
+        restaurants.add(new Restaurant (null, "Napoli", "napoli@email.com", 888777444, "Calle Bota 11", "Barcelona", 23453, "www.napoli.com"));
+        restaurants.add(new Restaurant (null, "Roma", "roma@email.com", 111222333, "Calle Clavo 56", "Alicante", 89726, "www.roma.com"));
+        restaurants.add(new Restaurant (null, "Funghi", "funghi@email.com", 776655443, "Calle Champiñon 36", "Madrid", 34567, "www.funghi.com"));
+
+        model.addAttribute("restaurant", restaurants);
+
         return "restaurants.html";
     }
 }
