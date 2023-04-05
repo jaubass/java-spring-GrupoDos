f27ff083169ee00a1fc25f1e1228bf54b0874a62
AlexPalacios24 <apalaciosleo@uoc.edu>
2023-04-05 10:53:13 +0200

    Inicio de desarrollo

--- README.md
+++
@@ -1,4 +0,0 @@
-# Proyecto con Spring Boot del GrupoDos
-### Programación Back-End con servidores de aplicaciones de la xUOC
-
-El proyecto supone la inmersión en la tecnología Spring para el desarrollo de aplicaciones en servidores con Java.
--- src/main/java/com/grupoDos/JavaBackendProject/repository/RestaurantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grupoDos.JavaBackendProject.repository;
-
-import com.grupoDos.JavaBackendProject.model.Restaurant;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RestaurantRepository extends JpaRepository <Restaurant, Long>{
-}
