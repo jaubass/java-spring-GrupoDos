e0903e3efb1c93292bf1a3d533c6d076e32c4776
pquintanap <116024429+pquintanap@users.noreply.github.com>
2023-04-25 18:46:50 +0100

    Update RoleService.java

--- src/main/java/com/grupoDos/JavaBackendProject/service/RoleService.java
+++ src/main/java/com/grupoDos/JavaBackendProject/service/RoleService.java
@@ -1,7 +1,13 @@
 package com.grupoDos.JavaBackendProject.service;
 
-
 import com.grupoDos.JavaBackendProject.model.Role;
+import com.grupoDos.JavaBackendProject.repository.RoleRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoleService {
