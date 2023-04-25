028b9a01c8b49e393078ccb323a4805b6b5a1f4a
pquintanap <116024429+pquintanap@users.noreply.github.com>
2023-04-25 18:17:29 +0100

    Update Role.java

--- src/main/java/com/grupoDos/JavaBackendProject/model/Role.java
+++ src/main/java/com/grupoDos/JavaBackendProject/model/Role.java
@@ -4,40 +4,52 @@ import jakarta.persistence.*;
 
 @Entity
 public class Role {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
-    @Column
+    private Long id;
+    
     private String name;
-
-    @Column
+    
     private String description;
-
-    public Role(){}
-
-    public long getId() {
+    
+    public Role() {
+        
+    }
+    
+    public Role(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+    
+    public Long getId() {
         return id;
     }
-
-    public void setId(long id) {
+    
+    public void setId(Long id) {
         this.id = id;
     }
-
+    
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
-
+    
     public String getDescription() {
         return description;
     }
-
+    
     public void setDescription(String description) {
         this.description = description;
     }
+    
+    @Override
+    public String toString() {
+        return "Role [id=" + id + ", name=" + name + ", description=" + description + "]";
+    }
+    
 }
