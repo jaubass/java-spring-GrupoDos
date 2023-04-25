34ce7e46c341e0e6b00b117f0976f7c95b5fcc34
pquintanap <116024429+pquintanap@users.noreply.github.com>
2023-04-25 18:43:58 +0100

    Update RoleRepository.java

--- src/main/java/com/grupoDos/JavaBackendProject/repository/RoleRepository.java
+++ src/main/java/com/grupoDos/JavaBackendProject/repository/RoleRepository.java
@@ -1,7 +1,7 @@
 package com.grupoDos.JavaBackendProject.repository;
 
 import com.grupoDos.JavaBackendProject.model.Role;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
