bb3e894001453ef8d02b5faef657d7e8471dab39
pquintanap <116024429+pquintanap@users.noreply.github.com>
2023-04-25 18:40:30 +0100

    Update RoleRepository.java

--- src/main/java/com/grupoDos/JavaBackendProject/repository/RoleRepository.java
+++ src/main/java/com/grupoDos/JavaBackendProject/repository/RoleRepository.java
@@ -5,6 +5,14 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepository extends CrudRepository<Role, Long> {
-    Role findRoleByName(String name);
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name); // buscar un rol por nombre
+
+    List<Role> findByDescriptionContaining(String keyword); // buscar roles por descripción que contengan una palabra clave
+
+    List<Role> findByOrderByDescriptionAsc(); // buscar todos los roles ordenados por descripción de forma ascendente
+
+    List<Role> findByOrderByDescriptionDesc(); // buscar todos los roles ordenados por descripción de forma descendente
+
 }
