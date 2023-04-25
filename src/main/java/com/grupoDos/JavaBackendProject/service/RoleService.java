e992d8b1f44cff172df4e71faa49d9255fcf4862
pquintanap <116024429+pquintanap@users.noreply.github.com>
2023-04-25 18:45:55 +0100

    Update RoleService.java

--- src/main/java/com/grupoDos/JavaBackendProject/service/RoleService.java
+++ src/main/java/com/grupoDos/JavaBackendProject/service/RoleService.java
@@ -3,6 +3,30 @@ package com.grupoDos.JavaBackendProject.service;
 
 import com.grupoDos.JavaBackendProject.model.Role;
 
-public interface RoleService {
-    Role findByName(String name);
+@Service
+public class RoleService {
+    
+    @Autowired
+    private RoleRepository roleRepository;
+    
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
+    
+    public Optional<Role> getRoleById(Long id) {
+        return roleRepository.findById(id);
+    }
+    
+    public Role createRole(Role role) {
+        return roleRepository.save(role);
+    }
+    
+    public Role updateRole(Role role) {
+        return roleRepository.save(role);
+    }
+    
+    public void deleteRoleById(Long id) {
+        roleRepository.deleteById(id);
+    }
+    
 }
